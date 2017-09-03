2120b42994a058aef00ab71d0a89f059da87b741
mr-skyaakash <aakash.jha@btech.christuniversity.in>
2017-09-03 09:37:53 +0530

    Add project files.

---
+++ Elections/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Elections
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCon sq = new SqlCon();
+            sq.open_con();
+            DataSet ds = sq.data_access("select * from LoginCred");
+
+            String name = "";
+            for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                if (textBox1.Text.Equals(ds.Tables[0].Rows[i].ItemArray[0]) && textBox2.Text.Equals(ds.Tables[0].Rows[i].ItemArray[1]))
+                {
+                    name = ds.Tables[0].Rows[i].ItemArray[2].ToString();
+                    MessageBox.Show("Welcome " + name);
+                }
+            }
+            this.Close();
+            Form2 frm = new Elections.Form2(name);
+            frm.Show();
+
+            sq.close_con();
+            
+        }
+    }
+}
---
+++ Elections/Sqlcon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Elections
+{
+    class SqlCon
+    {
+        static String connectionString = "Data Source=172.16.210.250,49170;Initial Catalog=Elections;Integrated Security=True";
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter adt;
+
+        public void open_con()
+        {           
+                con = new SqlConnection(connectionString);
+                con.Open();            
+        }
+
+        public void close_con()
+        {
+            con.Close();
+            adt.Dispose();
+        }
+
+        public void exec_query(String query)
+        {
+            cmd = new SqlCommand(query, con);
+            //cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+        }
+
+        public DataSet data_access(String query)
+        {
+            adt = new SqlDataAdapter(query, con);
+            DataSet ds = new DataSet();
+            adt.Fill(ds);
+            return ds;
+        }
+
+        public void validatelogin()
+        {
+            open_con();
+        }
+    }
+}
